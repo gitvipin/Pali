cf0b584c545cb69fcce90480e8e317c725d53168
Vipin Sharma <sh.vipin@gmail.com>
2019-02-12 22:07:43 -0800

    Pavo : Documentation on Tasks.
    Commit includes some documentation around Task class.

--- projects/pavo/src/assembly.py
+++ projects/pavo/src/assembly.py
@@ -1,10 +1,54 @@
 #!/usr/bin/env python
+'''
+This module defines the Abstract Interface and Implementation for
+Pipeline Stages, Pipelines and Assembly.
 
+A Stage is an atomic step as part of whole pipelining process. A Pipeline
+is a collection of Stages. An Assembly is collection of Pipelines.
 
+There can be several stages in a pipeline but there must be a serial order
+in which they shall be executed. This is the reason Stages are kept in the
+form of Linked List inside a pipeline. No two stages of same pipeline can
+be executed in parallel. However, there can be multiple pipelines
+that can be run in parallel.
+
+A single thread shall be assigned to execute a pipeline stage as it
+is always executed in serial order.
+
+Pipelines are nothing but a description of collection of steps to be
+executed on a given set of data.
+
+One usecase of pipelines is to configure endpoints / VMs / nodes. Here,
+set of steps to be executed is same for all the endpoints but need to be
+separately and can also be executed in parallel. In such a case, a pipeline
+will be defined once but Assembly will create multiple Pipelines with varying
+IP addresses of endpoints to be configured.
+
+'''
+
+import abc
+import logging
 import task as task
 
 class Stage(task.Task):
-    pass
+
+    """
+    Stages are like the atomic step of pipelines execution.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        self._next= None
+        self.log = logging.getLogger(__name__)
+
+    @abc.abstractmethod
+    def _run(self, data):
+        """
+        override this.  The data map can be used to pass runtime messages
+        to the next stage
+        """
+        pass
 
 
 class Pipeline(object):
--- projects/pavo/src/task.py
+++ projects/pavo/src/task.py
@@ -2,12 +2,28 @@
 '''
 This module represents the Basic Task in Pavo system.
 '''
+import abc
 
 class Task(object):
     (NEW, READY, RUNNING, WAITING, FINISHED) = range(5)
+    __metaclass__ = abc.ABCMeta
 
     def __init__(self):
+        """
+        This class defines the abstract representation of Task in Pavo system.
+
+        A Task has a well defined life cycle and is treated accordingly. All
+        the Tasks in the Pavo system MUST be derived from it. This class in
+        itself is non-instantiable with the help of abstract method 'run' and
+        '_run'.
+
+        A Task object should be self contained and it's run method shouldn't
+        accept any arguments. Any attribute, metadata, configuration or
+        properties needed for the task must be passed in the contrsuctor at the
+        time of initialization.
+        """
         self._state = self.NEW
+        super(Task, self).__init__()
 
     @property
     def state(self):
@@ -23,18 +39,29 @@ class Task(object):
         del self._state
 
     def run(self):
-        # TODO : make it non overridable
+        """
+        This method exposes the interface for running the task. This is a
+        non overridable method in class so as derived class types do not
+        override the default behavior of execution of tasks in the system.
+        """
         try:
             self._run()
         except Exception as err:
             # TODO: Do Task Failure to run exception handling
             pass
 
+    @abc.abstractmethod
     def _run(self):
+        """
+        A helper 'run' routine for running Tasks in pavo system. The core
+        functionality of task will be run through this helper routine.
+        """
         pass
 
-
-
 if __name__ == '__main__':
-    t = Task()
+    class MyTask(Task):
+        def _run(self):
+            pass
+
+    t = MyTask()
     t.run()
