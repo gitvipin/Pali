a2a675e98a8adc26766aae5e2ed76fc83e61dcde
Vipin Sharma <sh.vipin@gmail.com>
2022-08-07 17:37:33 -0700

    pali: Add Producer Consumer pattern
    Adds a generic implementation of ProducerConsumer pattern a.k.a.
    BoundedBuffer. Applications need to :
    - inherit from ProducerConsumer class
    - override produce/consume methods
    - start operations.

---
+++ examples/bbuffer.py
@@ -0,0 +1,59 @@
+'''
+This module demonstrates examples of ProducerConsumer and
+ThreadTaskLoop
+'''
+import time
+
+from pali.bbuffer import ProducerConsumer
+from pali.logger import setup_logging
+from pali.thread import ThreadTaskLoop
+
+class TimeStampRecorder(ProducerConsumer):
+    """
+    A simple application of Bounded Buffer a.k.a ProducerConsumer.
+    Producer generates timestamps every 2 seconds. 
+    Consumer simply consumers timestamps generated and prints them on
+    stdout.
+
+    """
+    NAME = "TimestampRecorder"
+
+    def produce(self):
+        """Generate timestamp every 2 seconds"""
+        time.sleep(1)
+        self.add_to_buffer(int(time.time()))
+
+    def consume(self):
+        """ Print timestamp every 1 second from buffer"""
+        time.sleep(2)
+        data = self.pop_from_buffer()
+        print (data)
+        print ("Buffer size : ", self.get_buffer_size())
+
+
+class Producer(ThreadTaskLoop):
+    """
+    A simple ThreadTaskLoop that will run '_task' method
+    in a loop.
+    """
+    array = []
+
+    def _task(self):
+        time.sleep(1)
+        self.array.append(int(time.time()))
+
+
+if __name__ == '__main__':
+    setup_logging()
+
+    # Create an instance of Producer and once started, 
+    # We should see timestamps collected in it's
+    # internal storage.
+    pc = Producer()
+    pc.start()
+
+    # Create a timestamp recorder and
+    tc = TimeStampRecorder()
+    tc.start()
+    time.sleep(10)
+    tc.stop()
